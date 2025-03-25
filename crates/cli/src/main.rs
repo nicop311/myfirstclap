bfefefdb85a5ee4723d2f413d6896e66b6ee4b5e
nicop311 <18116277+nicop311@users.noreply.github.com>
2025-03-25 15:21:05 +0100

    Start using ValueEnum to list all the possible_values that a CLI clap flag can take using the auto completion scrips
    Signed-off-by: nicop311 <18116277+nicop311@users.noreply.github.com>

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,27 +1,81 @@
-use clap::{Parser, Subcommand, CommandFactory};
+use clap::{Parser, Subcommand, CommandFactory, ValueEnum};
 use clap_complete::{generate, Shell};
 use anyhow::Result;
 use std::env;
 use std::io;
 use pretty_env_logger;
 
+/// a local enum that mirrors the log::Level enum and derives the necessary traits
+#[derive(ValueEnum, Clone)]
+enum LogLevel {
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+/// implement the ToString trait for the VersionOutputFormats enum
+impl ToString for LogLevel {
+    fn to_string(&self) -> String {
+        match self {
+            LogLevel::Error => "error".to_string(),
+            LogLevel::Warn =>  "warn".to_string(),
+            LogLevel::Info =>  "info".to_string(),
+            LogLevel::Debug => "debug".to_string(),
+            LogLevel::Trace => "trace".to_string(),
+        }
+    }
+}
+
 #[derive(Parser)]
 #[command(name = "myfirstclap")]
 #[command(version = version::get_describe())]
 struct Cli {
-    #[arg(long, default_value = "info", help = "Set the log verbosity level (error, warn, info, debug, trace)")]
-    log_level: String,
+    #[arg(long, 
+        default_value = "info",
+        value_enum,
+        help = format!("Set the log verbosity level ({}, {}, {}, {}, {})", 
+                        LogLevel::Error.to_string(),
+                        LogLevel::Warn.to_string(),
+                        LogLevel::Info.to_string(),
+                        LogLevel::Debug.to_string(),
+                        LogLevel::Trace.to_string() ))]
+        log_level: LogLevel,
 
     #[command(subcommand)]
     command: Commands,
+}
+
+/// Supported output formats for the version CLI command
+#[derive(ValueEnum, Clone)]
+enum VersionOutputFormat {
+    Text,
+    Json,
+    Full,
+}
+
+/// implement the ToString trait for the VersionOutputFormats enum
+impl ToString for VersionOutputFormat {
+    fn to_string(&self) -> String {
+        match self {
+            VersionOutputFormat::Text => "text".to_string(),
+            VersionOutputFormat::Json => "json".to_string(),
+            VersionOutputFormat::Full => "full".to_string(),
+        }
+    }
 }
 
 #[derive(Subcommand)]
 enum Commands {
     /// A more detailed version command with information from the build.
     Version {
-        #[arg(short, long, default_value = "text", help = "Supported values: text, json, full.")]
-        output: String,
+        #[arg(short,
+            long,
+            default_value = "text",
+            value_enum,
+            help = "Supported values: text, json, full.")]
+        output: VersionOutputFormat,
     },
     /// A collection of several trivial servers
     Serve {
@@ -30,7 +84,11 @@ enum Commands {
     },
     /// Completion scripts for various terminals.
     Completion {
-        #[arg(short, long, default_value = "bash", help = "Supported values: bash.")]
+        #[arg(short,
+            long,
+            default_value = "bash",
+            value_enum,
+            help = "Supported values: bash, elvish, fish, powershell, zsh.")]
         shell: Shell,
     },
 }
@@ -44,11 +102,11 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let cli = Cli::parse();
 
     // Initialize logging and set log level
-    env::set_var("RUST_LOG", &cli.log_level);
+    env::set_var("RUST_LOG", &cli.log_level.to_string());
     pretty_env_logger::init_timed();
 
     match cli.command {
-        Commands::Version { output } => version::run(output)?,
+        Commands::Version { output } => version::run(output.to_string())?,
         Commands::Serve { command } => match command {
             ServeCommands::Hello => serve::hello::run()?,
         },
